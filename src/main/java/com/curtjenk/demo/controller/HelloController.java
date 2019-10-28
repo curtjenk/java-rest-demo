dd50a972eb0d0c6f3d085e6ceb7319a65e94e5a3
Curtis Jenkins <>
2019-10-27 20:54:56 -0400

    ditto

--- src/main/java/com/curtjenk/demo/controller/HelloController.java
+++ src/main/java/com/curtjenk/demo/controller/HelloController.java
@@ -1,22 +1,19 @@
 package com.curtjenk.demo.controller;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.curtjenk.demo.dto.UserDto;
 import com.curtjenk.demo.service.UserService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    // private static Logger logger =
+    // LoggerFactory.getLogger(HelloController.class);
 
     private UserService userService;
 
@@ -29,12 +26,6 @@ public class HelloController {
 
         return userService.getAllUsers();
 
-    }
-
-    private String show(String... x) {
-        String line = Stream.of(x).collect(Collectors.joining(" "));
-        System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " " + line);
-        return line;
     }
 
 }
