ebfbb85ecf06e958cc45d2c87274ced3e390f6fa
Curtis Jenkins <>
2019-11-04 20:45:40 -0500

    skip tests

--- src/main/java/com/curtjenk/demo/security/WebSecurityConfig.java
+++ src/main/java/com/curtjenk/demo/security/WebSecurityConfig.java
@@ -10,12 +10,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /*
 This class extends the WebSecurityConfigurerAdapter. 
